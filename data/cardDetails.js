118c6de7e0a1f03f0e540bad3a9ffe8801013212
Prithvi Raj Vuppalapati <prithvirajvuppalapati@l-285000883.wipro.com>
2018-10-09 21:02:22 +0530

    data changed

--- data/cardDetails.js
+++ data/cardDetails.js
@@ -3,17 +3,19 @@ module.exports = {
         {
             username : 'alice', 
             cardData : {
-                cardType: 'LLOYDS BANK CURRENT ACCOUNT',
-                cardTitle: 'CClub Lloyds Platinum Account',
-                aer: '1.5'
-            },
+                cardType: 'LLOYDS BANK SAVINGS ACCOUNT',
+                cardTitle: 'LBG - Club Llyods Saver Account',
+                aer: '0.6',
+                savingsOnBestMatch: '4,627.60'
+            }
         },
         {   
             username : 'john',
             cardData : {
                 cardType: 'LLOYDS BANK PLATINUM CREDIT CARD',
                 cardTitle: '0% Purchase and Balance transfer',
-                apr: '19.9'
+                apr: '19.9',
+                savingsOnBestMatch: '149'
             }
         }
     ]
