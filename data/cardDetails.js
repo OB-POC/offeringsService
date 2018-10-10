e4ace1d6fdc6492430bda60932ee3127587ff1f9
Prithvi Raj Vuppalapati <prithvirajvuppalapati@Prithvis-MacBook-Pro.local>
2018-10-10 18:32:55 +0530

    data changes

--- data/cardDetails.js
+++ data/cardDetails.js
@@ -3,19 +3,24 @@ module.exports = {
         {
             username : 'alice', 
             cardData : {
+                typeLeftOut: 'debit',
                 cardType: 'LLOYDS BANK SAVINGS ACCOUNT',
                 cardTitle: 'LBG - Club Llyods Saver Account',
                 aer: '0.6',
-                savingsOnBestMatch: '4,627.60'
+                savingsOnBestMatch: '4,828.80'
             }
         },
         {   
             username : 'john',
             cardData : {
+                typeLeftOut: 'credit',
                 cardType: 'LLOYDS BANK PLATINUM CREDIT CARD',
                 cardTitle: '0% Purchase and Balance transfer',
                 apr: '19.9',
-                savingsOnBestMatch: '149'
+                savingsOnBestMatch: '258',
+                introductoryTerm: '20M',
+                transferFee: 0,
+                afterIntroductoryMonthlySavings: 13 
             }
         }
     ]
